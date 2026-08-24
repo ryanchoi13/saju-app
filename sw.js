a1eb30afcf295ef6455732f0fa47d7f947ba159b
ryanchoi13 <jungohchoi13@gmail.com>
2026-08-24 15:58:51 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,17 @@
-const CACHE_NAME = 'saju-app-v1';
-
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
+// 이전 캐시 강제 무효화 및 네트워크 우선 연결
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim());
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(keys.map((key) => caches.delete(key)));
+        }).then(() => self.clients.claim())
+    );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+self.addEventListener('fetch', (event) => {
+    // 캐시 없이 항상 최신 서버 파일 호출
+    event.respondWith(fetch(event.request));
 });
